cfb3ffa144038e696752e7004645610216056df8
AlessandroFP <alessandrofp30@gmail.com>
2018-07-16 23:52:53 -0500

    Aplication Code

---
+++ app/src/main/java/com/tekshia/airline/Entity/Purchase.java
@@ -0,0 +1,11 @@
+package com.tekshia.airline.Entity;
+/*
+* Developed By @KosKou
+* Go to: github.com/koskou
+* Enjoy :D
+* */
+public class Purchase {
+    //Url Default
+    public static String Url = "jsonapplication.000webhostapp.com";
+   // public static String Url = "192.168.192.2:80";
+}
